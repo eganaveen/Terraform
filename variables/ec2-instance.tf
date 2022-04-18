be530fb26af9cf22f97e5ac76fc4f6acc871db47
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-18 15:06:41 +0530

    Updating Terraform

--- variables/ec2-instance.tf
+++ variables/ec2-instance.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.0"
+#    }
+#  }
+#}
 
 resource "aws_instance" "ec2-instance" {
   ami = "ami-0bb6af715826253bf"
